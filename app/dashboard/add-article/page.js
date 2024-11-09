00006da0ce12c886768beeba0c2cf890bf7ff941
RashedAbdullah <maarashed40@gmail.com>
2024-11-09 20:14:02 +0600

    add artilce page implemented

--- app/dashboard/add-article/page.js
+++ app/dashboard/add-article/page.js
@@ -1,5 +1,11 @@
+import AddArticleForm from "../_components/add-article-form";
+
 const AddArticlePage = () => {
-  return <div>AddArticlePage</div>;
+  return (
+    <div>
+      <AddArticleForm />
+    </div>
+  );
 };
 
 export default AddArticlePage;
---
+++ app/dashboard/update-article/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UpdateArticlePage = () => {
+  return (
+    <div>UpdateArticlePage</div>
+  )
+}
+
+export default UpdateArticlePage
